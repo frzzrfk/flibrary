bc38551a52644a78c641ef9106283d95563a66e1
frzrfk <uamanb314@gmail.com>
2023-11-25 10:44:52 +0500

    Initial Setup

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use (expressLayouts);
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL);
+//mongoose.connect(process.env.DATABASE_URL, {
+//useNewUrlParser: true })
 
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
